7d0c8b79c22f648cd647feea0cdd8161d9e2d456
Atchett <Atchett@users.noreply.github.com>
2019-11-01 11:29:54 +0000

    working through examples, implement blog, implement paging, templates etc

--- src/templates/post-template.js
+++ src/templates/post-template.js
@@ -1,8 +1,25 @@
 import React from "react"
+import { graphql } from "gatsby"
+
 import Layout from "../components/layout"
 
-export default () => (
+export default ({ data: post }) => (
   <Layout>
-    <h2>Programattically created Blog Post</h2>
+    <div>
+      <h1>{post.markdownRemark.frontmatter.title}</h1>
+      <div dangerouslySetInnerHTML={{ __html: post.markdownRemark.html }}></div>
+    </div>
   </Layout>
 )
+
+export const query = graphql`
+  query($slug: String!) {
+    markdownRemark(fields: { slug: { eq: $slug } }) {
+      html
+      frontmatter {
+        title
+        date
+      }
+    }
+  }
+`
